66d7185d7ae8b6e51a9711db95f763e15b1f5953
Ryan Leyva <ryan.c.leyva@gmail.com>
2022-06-18 14:02:45 -0500

    first commit

---
+++ src/main/java/leyva/ryan/baseballstatsapi/BaseballStatsApiApplication.java
@@ -0,0 +1,13 @@
+package leyva.ryan.baseballstatsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballStatsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leyva/ryan/baseballstatsapi/BaseballStatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package leyva.ryan.baseballstatsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballStatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
