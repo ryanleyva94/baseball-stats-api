1fdccbcd45cb3984cc0f88091cd7344850f4ab8e
Ryan Leyva <ryan.c.leyva@gmail.com>
2022-06-18 18:24:31 -0500

    Added README and removed unused import

---
+++ README.md
@@ -0,0 +1,3 @@
+This application processes statistics about baseball players and presents them via an API. 
+
+It consumes messages about lists of the players in a given game then persists those messages to a database. An endpoint allows for one to see the season-to-date totals and stats for a given player.
--- src/main/java/leyva/ryan/baseballstatsapi/config/AppConfig.java
+++ src/main/java/leyva/ryan/baseballstatsapi/config/AppConfig.java
@@ -9,7 +9,6 @@ import leyva.ryan.baseballstatsapi.services.GameAddingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
 public class AppConfig
